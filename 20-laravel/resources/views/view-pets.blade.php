abef20ed2b5e447a93aa78a6949fbc3e664c2ba3
Juca2706 <juncamilo.hg78@gmail.com>
2025-11-06 11:56:12 -0500

    instalacion de composer breeze y creacion de la vista individual de las mascotas

--- 20-laravel/resources/views/view-pets.blade.php
+++ 20-laravel/resources/views/view-pets.blade.php
@@ -13,8 +13,9 @@
     <section class="p-10 flex gap-4 flex-wrap justify-center">
         @foreach($pets as $pet)
             <div class="card card-side bg-base-100 shadow-sm ">
-            <figure class="w-[140px]">
-                <img
+            <figure class="w-[200px] h-[200px]">
+                <img 
+                    class="object cover"
                     src="{{ asset('images/'. $pet->image) }}"
                     alt="{{ $pet->image }}" />
             </figure>
@@ -35,8 +36,9 @@
                 
                 <p class="w-[200px]">{{ $pet->description }}</p>
                 <div class="card-actions justify-end">
-                <a class="text-white bg-teal-700 rounded-full p-1 hover:scale-120 transition-all" href="">
+                <a class="text-white bg-teal-700 rounded-full flex items-center gap-2 py-1 px-4 hover:scale-120 transition-all" href="{{ url('view/pet/'.$pet->id) }}">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M247.31,124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57,61.26,162.88,48,128,48S61.43,61.26,36.34,86.35C17.51,105.18,9,124,8.69,124.76a8,8,0,0,0,0,6.5c.35.79,8.82,19.57,27.65,38.4C61.43,194.74,93.12,208,128,208s66.57-13.26,91.66-38.34c18.83-18.83,27.3-37.61,27.65-38.4A8,8,0,0,0,247.31,124.76ZM128,192c-30.78,0-57.67-11.19-79.93-33.25A133.47,133.47,0,0,1,25,128,133.33,133.33,0,0,1,48.07,97.25C70.33,75.19,97.22,64,128,64s57.67,11.19,79.93,33.25A133.46,133.46,0,0,1,231.05,128C223.84,141.46,192.43,192,128,192Zm0-112a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160Z"></path></svg>
+                    <strong>View</strong>
                 </a>         
                 </div>
              </div>
