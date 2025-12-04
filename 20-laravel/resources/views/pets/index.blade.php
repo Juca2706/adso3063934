a25ae08cdde1c4499822040b2dfed2c70e7a86bc
Juca2706 <juncamilo.hg78@gmail.com>
2025-12-04 07:14:19 -0500

    Arreglo de la aparicion del nombre de la mascota en el modal de confirmacion  para eliminar

--- 20-laravel/resources/views/pets/index.blade.php
+++ 20-laravel/resources/views/pets/index.blade.php
@@ -197,7 +197,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
             </svg>
-            <span>You want to delete: <strong class="name"></strong></span>
+            <span>You want to delete: <strong class="fullname"></strong></span>
         </div>
         <div class="flex gap-2 mt-4">
             <button class="btn btn-outline btn-success btn-confirm btn-sm">Delete</button>
@@ -223,8 +223,8 @@
 
         // Delete User
         $('table').on('click', '.btn-delete', function () {
-            $name = $(this).data('name')
-            $('.name').text($name)
+            $fullname = $(this).data('fullname')
+            $('.fullname').text($fullname)
             $frm = $(this).next()
             modal_delete.showModal()
         })
